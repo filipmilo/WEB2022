fb9d94e4d4df0be46669f3f3ef5aea26e4b0a63a
Stefan Pekez <stefanpekez5@gmail.com>
2022-06-17 14:46:21 +0200

    Added register frontend

---
+++ static/js/app.js
@@ -0,0 +1,28 @@
+const Register = {template: '<register></register>' }
+
+const router = new VueRouter({
+	mode: 'hash',
+	  routes: [
+		{ path: '/', component: Register}
+	  ]
+});
+
+var app = new Vue({
+	router,
+	el: '#vue-app'
+});
+
+/*var app = new Vue({
+	el: '#vue-app',
+	data: {
+		name: 'Stefan',
+		job: 'Ninja',
+		website: 'https://www.google.com/',
+		websiteTag:'<a href="https://www.google.com/">WebsiteTag</a>'
+	},
+	methods: {
+		greet: function(time){
+			return 'Good ' + time + ' ' + this.name;
+		}
+	}
+})*/
