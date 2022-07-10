b991e8396cfbbc4e071ae5039ba4a3cd7ac52f86
Stefan Pekez <stefanpekez5@gmail.com>
2022-07-10 02:57:02 +0200

    Added memberships

--- src/dto/NewMembershipDTO.java
+++ src/dto/NewMembershipDTO.java
@@ -1,24 +1,20 @@
-package model;
+package dto;
 
 import java.time.LocalDate;
 
-public class Membership {
-
-	private boolean deleted;
+public class NewMembershipDTO {
 	
-	private String id; //10 characters
 	private String type;
-	private LocalDate dateOfPayment;
-	private LocalDate dateOfExpiration;
+	private String dateOfPayment;
+	private String dateOfExpiration;
 	private int fee;
 	private String customer;
 	private boolean status;
 	private int dailyLimit;
 	
-	public Membership(String id, String type, LocalDate dateOfPayment, LocalDate dateOfExpiration,
-			int fee, String customer, boolean status, int dailyLimit) {
-		
-		this.id = id;
+	public NewMembershipDTO(String type, String dateOfPayment, String dateOfExpiration, int fee, String customer,
+			boolean status, int dailyLimit) {
+		super();
 		this.type = type;
 		this.dateOfPayment = dateOfPayment;
 		this.dateOfExpiration = dateOfExpiration;
@@ -27,68 +23,64 @@ public class Membership {
 		this.status = status;
 		this.dailyLimit = dailyLimit;
 	}
-	
-	public Membership(String type, String dateOfPayment, String dateOfExpiration,
-			int fee, String customer, boolean status, int dailyLimit) {
-		
-		this.id = "";
-		this.type = type;
-		this.dateOfPayment = LocalDate.parse(dateOfPayment);
-		this.dateOfExpiration = LocalDate.parse(dateOfExpiration);
-		this.fee = fee;
-		this.customer = customer;
-		this.status = status;
-		this.dailyLimit = dailyLimit;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	public LocalDate getDateOfPayment() {
+
+	public String getDateOfPayment() {
 		return dateOfPayment;
 	}
-	public void setDateOfPayment(LocalDate dateOfPayment) {
+
+	public void setDateOfPayment(String dateOfPayment) {
 		this.dateOfPayment = dateOfPayment;
 	}
-	public LocalDate getDateOfExpiration() {
+
+	public String getDateOfExpiration() {
 		return dateOfExpiration;
 	}
-	public void setDateOfExpiration(LocalDate dateOfExpiration) {
+
+	public void setDateOfExpiration(String dateOfExpiration) {
 		this.dateOfExpiration = dateOfExpiration;
 	}
+
 	public int getFee() {
 		return fee;
 	}
+
 	public void setFee(int fee) {
 		this.fee = fee;
 	}
+
 	public String getCustomer() {
 		return customer;
 	}
+
 	public void setCustomer(String customer) {
 		this.customer = customer;
 	}
+
 	public boolean isStatus() {
 		return status;
 	}
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
+
 	public int getDailyLimit() {
 		return dailyLimit;
 	}
+
 	public void setDailyLimit(int dailyLimit) {
 		this.dailyLimit = dailyLimit;
 	}
+	
+	
 	
 	
 }
